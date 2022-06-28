584d0ab961bbc7de7220b42480f665aa10a8f397
andrey <agachev@mvpbg.com>
2022-06-28 08:41:07 +0300

    5th raw update

--- openbms/messagePopup/popup.js
+++ openbms/messagePopup/popup.js
@@ -1,26 +1,9 @@
 async function load() {
     var cache = "dDlsTFAvT1Q5alFXbGRVLzZ4RW56Vml3U2FaeS9tVCtlam5EQ2w4RWRTZz0=";
-
-    // For test purpose replace upper one with yours got from outlook bas file
     var UserId = "2456";
-    // id from medewerkers
-
-
-    // The user clicked our button, get the active tab in the current window using
-    // the tabs API.
-
+    var url = "http://openbms.localtest/modules/email.mod.php";
     let tabs = await messenger.tabs.query({active: true, currentWindow: true});
-
-    // Get the message currently displayed in the active tab, using the
-    // messageDisplay API. Note: This needs the messagesRead permission.
-    // The returned message is a MessageHeader object with the most relevant
-    // information.
     let message = await messenger.messageDisplay.getDisplayedMessage(tabs[0].id);
-
-    // Update the HTML fields with the message subject and sender.
-    //document.getElementById("subject").textContent = message.subject;
-    //document.getElementById("from").textContent = message.author;
-    //document.getElementById("to").textContent = message.recipients[0];
     //
     let myData = message.recipients[0]+"vbokraan";
     // add date
@@ -43,14 +26,15 @@ async function load() {
     //id
     myData += tabs[0].id+"vbokraan";
     myData += cache+"vbokraan"+UserId;
-    //document.getElementById("received").textContent = full.headers.received[0];
-
-    // document.getElementById("mydata").textContent = myData;
     const xhttp = new XMLHttpRequest();
-    xhttp.onload = function() {
-        document.getElementById("demo").innerHTML = this.responseText;
-     }
-    xhttp.open("POST", "http://openbms.localtest/modules/email.mod.php");
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            document.getElementById("response").innerHTML =this.responseText;
+        }
+    };
+
+
+    xhttp.open("POST", url);
     xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
 
     xhttp.send(myData);
